0d207482dd1d646f008809630090ce6cc79065e3
diyorbekdevtuit <student1189@tuit.uz>
2025-10-25 18:52:28 +0500

    j

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import logo from '../assets/logo.png'
 import download from '../assets/download.png'
 import Facebook from '../assets/Facebook.png'
-import Tiktok from '../assets/Tiktok.png'
 import Instagram from '../assets/Instagram.png'
 import Snapchat from '../assets/Snapchat.png'
 
